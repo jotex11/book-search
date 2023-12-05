c8d0886c6ff05386585dd30a98bf44b213c0a7fc
joseph <jomill0723@outlook.com>
2023-12-04 21:54:46 -0500

    fixed vite errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: process.env.PORT || 3001,
     open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        secure: false,
+        secure: true,
         changeOrigin: true
       }
     }
